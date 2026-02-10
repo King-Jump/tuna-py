982ce699cc71f81a744340db1519bb92965d3671
caodonglei <donglei.cao@chainup.com>
2026-02-10 21:00:12 +0800

    ADD: market maker

---
+++ tunapy/maker/maker_libs.py
@@ -0,0 +1,230 @@
+""" Strategies and Tools for market making
+"""
+import time
+import logging
+import random
+
+from common.cexapi.restful_api import BatchOrder
+from tunapy.management.market_making import TokenParameter
+
+LOGGER = logging.getLogger('MM')
+
+def gen_ask_orders(
+    order_book: list,
+    param: TokenParameter,
+) -> list:
+    """ Generate ask orders
+    """
+    return _mirror_ask_orders(order_book, param)
+
+def gen_bid_orders(
+    order_book: list,
+    param: TokenParameter,
+) -> list:
+    """ Generate bid orders
+    """
+    return _mirror_bid_orders(order_book, param)
+
+def _mirror_ask_orders(
+    order_book: list,
+    param: TokenParameter,
+) -> list:
+    # the quantity discount in BPS
+    qty_coef = param.near_qty_multiplier
+    price_decimals = param.price_decimals
+    price_coef = 1. + 0.0001 * param.near_sell_price_margin
+
+    new_orders, count = [], 0
+    for ask, qty in order_book:
+        if count >= param.near_ask_size:
+            break
+        order_price = ask * price_coef
+        order_price = round(order_price, price_decimals) if price_decimals else int(order_price)
+        order_qty = _calc_maker_qty(order_price, qty * qty_coef, param)
+        if order_qty > 0:
+            new_orders.append((order_price, order_qty))
+            count += 1
+
+    return new_orders
+
+def _mirror_bid_orders(
+    order_book: list,
+    param: TokenParameter,
+) -> list:
+    # the quantity discount in BPS
+    qty_coef = param.near_qty_multiplier
+    price_decimals = param.price_decimals
+    price_coef = 1. + 0.0001 * param.near_buy_price_margin
+
+    new_orders, count = [], 0
+    for bid, qty in order_book:
+        if count >= param.near_bid_size:
+            break
+        order_price = bid * price_coef
+        order_price = round(order_price, price_decimals) if price_decimals else int(order_price)
+        order_qty = _calc_maker_qty(order_price, qty * qty_coef, param)
+        if order_qty > 0:
+            new_orders.append((order_price, order_qty))
+            count += 1
+
+    return new_orders
+
+def _calc_maker_qty(order_price: float, order_qty: float, param: TokenParameter):
+    max_amt_per_order = float(param.max_amt_per_order)
+    if order_qty * order_price > max_amt_per_order:
+        order_qty = max_amt_per_order / order_price
+    qty_decimals = param.qty_decimals
+    if qty_decimals == 0:
+        return int(order_qty)
+    return round(order_qty, qty_decimals)
+
+def gen_far_liquidity(symbol: str,
+                      param: TokenParameter,
+                      askbids: dict,
+                      side: str,
+                      guard_price: float,
+                      cl_order_start: int,
+) -> list:
+    """ generate far orders
+    """
+    offset = int(time.time() * 100) % 8640000
+    tif = param.far_tif
+    orders = []
+    if side == 'BUY':
+        # make more price margin if previous hedge loses
+        new_orders = _gen_bid_orders_far(askbids['bids'], param)
+        for price, qty in new_orders:
+            # avoid self-trade
+            if price < guard_price:
+                orders.append(BatchOrder(clorder_id=gen_client_order_id(
+                        f'B{symbol}', cl_order_start, offset, True),
+                              symbol=symbol,
+                              side="BUY",
+                              price=price,
+                              qty=qty,
+                              tif=tif))
+                offset += 1
+    else:
+        new_orders = _gen_ask_orders_far(askbids['asks'], param)
+        for price, qty in new_orders:
+            # avoid self-trade
+            if price > guard_price:
+                orders.append(BatchOrder(clorder_id=gen_client_order_id(
+                        f'S{symbol}', cl_order_start, offset, True),
+                              symbol=symbol,
+                              side="SELL",
+                              price=price,
+                              qty=qty,
+                              tif=tif))
+                offset += 1
+    return orders
+
+def _spread_far(
+    order_book: list, param: TokenParameter, side: str
+) -> list:
+    new_orders = []
+
+    base_price = order_book[0][0]
+    if side == 'SELL':
+        price_coef = 1 + 0.0001 * float(param.far_sell_price_margin)
+        max_size = param.far_ask_size
+    else:
+        price_coef = 1 - 0.0001 * float(param.far_buy_price_margin)
+        max_size = param.far_bid_size
+    price_decimals = param.price_decimals
+    qty_coef = param.far_qty_multiplier
+    qtys = [float(item[1]) for item in order_book]
+    qty_size = len(qtys)
+
+    for _ in range(max_size):
+        base_price *= price_coef
+        rand_idx = random.randrange(0, qty_size)
+        qty = qtys[rand_idx] * (0.95 + rand_idx * 0.05 / qty_size)
+
+        order_price = round(base_price, price_decimals) if price_decimals else int(base_price)
+        order_qty = _calc_maker_qty(order_price, qty * qty_coef, param)
+        if order_qty > 0:
+            new_orders.append((order_price, order_qty))
+    return new_orders
+
+def _gen_ask_orders_far(
+    order_book: list, # near order book, 20 top asks
+    param: TokenParameter,
+) -> list:
+    if param.far_strategy == 'spread':
+        return _spread_far(order_book, param, 'SELL')
+    return []
+
+def _gen_bid_orders_far(
+    order_book: list, # near order book, 20 top bids
+    param: TokenParameter,
+) -> list:
+    if param.far_strategy == 'spread':
+        return _spread_far(order_book, param, 'BUY')
+    return []
+
+
+def gen_client_order_id(
+    symbol: str, cl_order_start: int, cl_order_offset: int, far_end: bool = False
+) -> str:
+    """ Generate ClOrdID
+    """
+    if far_end:
+        # far-end liquidity
+        return f'F0{symbol}_{cl_order_start}_{cl_order_offset}'
+    return f'{symbol}_{cl_order_start}_{cl_order_offset}'
+
+def mix_ask_bid_orders(ask_orders: list, bid_orders: list) -> list:
+    """Mix ask and bid orders
+    """
+    ask_orders_len = len(ask_orders)
+    bid_orders_len = len(bid_orders)
+    mixed_orders = []
+
+    for ask_, bid_ in zip(ask_orders, bid_orders):
+        mixed_orders.append(ask_)
+        mixed_orders.append(bid_)
+    if bid_orders_len > ask_orders_len:
+        mixed_orders.extend(bid_orders[ask_orders_len:])
+    elif bid_orders_len < ask_orders_len:
+        mixed_orders.extend(ask_orders[bid_orders_len:])
+    return mixed_orders
+
+def _merge_orders(
+    prev_orders: list, new_orders: list, flags: list, cancel_ids: list, reserve_orders: list
+) -> list:
+    merged_orders = []
+    for idx, flag in enumerate(flags):
+        prev_order = prev_orders[idx]
+        if not flag:
+            # if False, replace previous order with new order
+            cancel_ids.append(prev_order.id)
+            merged_orders.append(new_orders[idx])
+        else:
+            reserve_orders.append(prev_order)
+    if len(prev_orders) > len(flags):
+        # remove additional previous orders
+        cancel_ids.extend([order.id for order in prev_orders[len(flags):]])
+    if len(new_orders) > len(flags):
+        # reverse additional new orders
+        merged_orders.extend(new_orders[len(flags):])
+    return merged_orders
+
+def diff_prev_new_orders(
+    diff_rate_per_round: float,
+    side: str,
+    prev_orders: list,
+    new_orders: list,
+    cancel_ids: list,
+    reserve_orders: list
+) -> list:
+    """ compare previous and new orders by price, if the difference < diff_rate_per_round,
+        reserve the previous order, otherwise cancel the previous order and reserve the new one.
+    """
+    # replace ask/bid order with large price difference
+    prev_orders.sort(key=lambda x: x.price, reverse=side=='BUY')  # sort by price
+    new_orders.sort(key=lambda x: x.price, reverse=side=='BUY')  # sort by price
+    # True if no price difference
+    flags = [abs(prev_order.price / order.price - 1) < diff_rate_per_round
+        for prev_order, order in zip(prev_orders, new_orders)]
+    return _merge_orders(prev_orders, new_orders, flags, cancel_ids, reserve_orders)
---
+++ tunapy/maker/market_maker.py
@@ -0,0 +1,329 @@
+""" Market Maker Main
+"""
+import os
+import sys
+import time
+import traceback
+import asyncio
+from logging import Logger
+from collections import namedtuple
+
+from tunapy.management.market_making import TokenParameter
+
+CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+BASE_PATH = os.path.dirname(CURR_PATH)
+if BASE_PATH not in sys.path:
+    sys.path.insert(0, BASE_PATH)
+
+from octopuspy.utils.log_util import create_logger
+from tunapy.management.redis_client import DATA_REDIS_CLIENT
+from maker.maker_libs import (
+    gen_ask_orders,
+    gen_bid_orders,
+    gen_client_order_id,
+    gen_far_liquidity,
+    mix_ask_bid_orders,
+    diff_prev_new_orders,
+)
+
+
+async def _clear_all_open_orders(symbol: str, ctx: dict, logger: Logger):
+    logger.info('Cancel all open orders of %s', symbol)
+    res = '' # TODO: cancel_all_by_symbol(symbol)
+    if not res:
+        logger.error('Can not cancel all open orders of %s', symbol)
+
+
+async def _clear_all_ner_open_orders(symbol: str, ctx: dict, logger: Logger):
+    logger.info("Cancel all ner open orders of %s", symbol)
+    orders = await _open_orders(ctx, symbol)
+    cancell_ids = [order['orderId']
+                   for order in orders if not order['clientOrderId'].startswith('F0')]
+    if cancell_ids:
+        res = '' # cancel_orders_by_ids(cancell_ids)
+        logger.info('Cancel all near orders %s', res)
+
+
+async def _make_orders(ctx: dict, symbol: str, orders: list, logger: Logger) -> list:
+    res = []
+    # TODO: get batch size from config
+    for start in range(0, len(orders), BATCH_SIZE):
+        sub_res = '' # batch_make_orders(orders[start:start + BATCH_SIZE])
+        logger.debug('Make Orders Response %s: %s', symbol, sub_res)
+        res.extend(sub_res)
+    return res
+
+async def _cancel_orders(ctx: dict, symbol: str, cancel_ids: list, logger: Logger) -> int:
+    cancel_num = 0
+    for start in range(0, len(cancel_ids), BATCH_SIZE):
+        sub_res = '' # cancel_orders_by_ids(cancel_ids[start:start + BATCH_SIZE], symbol)
+        logger.debug("cancel_orders %s: %s", symbol, sub_res)
+        cancel_num += len([oid for oid, status in sub_res['data'].items() if status == 'SUCCESS'])
+        if sub_res.get("code") != 200:
+            logger.error("cancel_orders: %s;error: %s",
+                         cancel_ids[start:start + BATCH_SIZE], sub_res)
+    return cancel_num
+
+async def _open_orders(ctx: dict, symbol: str) -> list:
+    return [] # TODO get_open_orders(symbol)
+
+async def handle_orders(
+    param: dict,
+    maker_symbol: str,
+    ask_orders: list,
+    bid_orders: list,
+    ctx: dict,
+    logger: Logger,
+    is_far: bool = False
+):
+    """ handle orders, including make and cancel orders
+    """
+    # scan top ask/bid of new orders
+    top_bid = bid_orders[0].price if bid_orders else 0
+    top_ask = ask_orders[0].price if ask_orders else sys.maxsize
+
+    # previous listed ask/bid orders, prev_asks/prev_bids are instance of CachedOrder
+    prev_asks = ctx.get('prev_farasks', []) if is_far else ctx.get('prev_asks', [])
+    prev_bids = ctx.get('prev_farbids', []) if is_far else ctx.get('prev_bids', [])
+    # the number of rounds without forcely refresh
+    no_force_refresh_num = ctx.get('no_force_refresh_num', 0)
+
+    # max difference between prices of sequent rounds
+    diff_rate_per_round = float(param.get('diff_rate_per_round', 0))
+    # diff_rate_per_round in BPS
+    diff_rate_per_round *= 0.0001
+    force_refresh_num = int(param.get('change_after_n_rounds', 6))
+
+    # order ids to be canceled
+    cancel_ids = []
+    # reserved previous orders
+    reserve_asks, reserve_bids = [], []
+    if diff_rate_per_round <= 0 or no_force_refresh_num >= force_refresh_num:
+        # forcely refresh: cancel all previous orders
+        cancel_ids = [co.id for co in prev_asks + prev_bids]
+        # reserve all new ask/bid orders
+        merged_asks = ask_orders
+        merged_bids = bid_orders
+        # update context
+        ctx['no_force_refresh_num'] = 0
+    else:
+        # replace ask/bid order with large price difference
+        merged_asks = diff_prev_new_orders(diff_rate_per_round, 'SELL', prev_asks, ask_orders,
+                                           cancel_ids, reserve_asks)
+        merged_bids = diff_prev_new_orders(diff_rate_per_round, 'BUY', prev_bids, bid_orders,
+                                           cancel_ids, reserve_bids)
+        # update context
+        ctx['no_force_refresh_num'] += 1
+
+    mix_new_orders = mix_ask_bid_orders(merged_asks, merged_bids)
+    logger.debug('New Ordres: %s', mix_new_orders)
+    cancel_num = 0 # number of canceled orders
+    made_orders = [] # response of make orders
+    if mix_new_orders:
+        # First put new orders, then cancel previous orders
+        made_orders = await _make_orders(ctx, maker_symbol, mix_new_orders, logger)
+        for order, item in zip(mix_new_orders, made_orders):
+            order_id = item.get("orderId")
+            if order_id:
+                if order.side == 'BUY':
+                    # TODO: define cancel order data structure
+                    reserve_bids.append(CachedOrder(price=order.price, id=order_id))
+                else:
+                    reserve_asks.append(CachedOrder(price=order.price, id=order_id))
+        if is_far:
+            ctx['prev_farasks'] = reserve_asks
+            ctx['prev_farbids'] = reserve_bids
+        else:
+            ctx['prev_asks'] = reserve_asks
+            ctx['prev_bids'] = reserve_bids
+
+        if cancel_ids:
+            cancel_num = await _cancel_orders(ctx, maker_symbol, cancel_ids, logger)
+            if not cancel_num:
+                # failed to cancel previous orders
+                ctx['top_ask'] = min(ctx.get('top_ask', top_ask), top_ask)
+                ctx['top_bid'] = max(ctx.get('top_bid', top_bid), top_bid)
+            elif not is_far:
+                # sucessful canceled, and near orders only
+                ctx['top_ask'] = top_ask
+                ctx['top_bid'] = top_bid
+        if is_far: # far end orders
+            # handle exception in batch make orders, roll-back
+            listed_orders = await _open_orders(ctx, maker_symbol)
+            # made_orders are orders of far-end
+            expect_ids = set([item.id for item in reserve_asks + reserve_bids])
+            # add previous near-end orders
+            for co in ctx.get('prev_asks', []):
+                expect_ids.add(co.id)
+            for co in ctx.get('prev_bids', []):
+                expect_ids.add(co.id)
+            unexpected_orders = [o['orderId'] for o in listed_orders if o['orderId'] \
+                                and o['orderId'] not in expect_ids]
+            if unexpected_orders:
+                await _cancel_orders(ctx, maker_symbol, unexpected_orders, logger)
+                logger.warning("Unexpected Orders %s", unexpected_orders)
+                logger.debug('listed orders: %s', listed_orders)
+    else:
+        # no new orders, only delete reduced orders.
+        if cancel_ids:
+            cancel_num = await _cancel_orders(ctx, maker_symbol, cancel_ids, logger)
+        ctx['no_force_refresh_num'] += 1
+
+    logger.debug("HANDLE ORDERS: symbol: %s, diff rate per round: %s, no force refresh rounds: %s, "
+                 "force refresh rounds: %s, prev ask size: %s, prev bid size: %s, "
+                 "news ask order size: %s, new bid order size: %s, update order size: %s",
+        maker_symbol, diff_rate_per_round, no_force_refresh_num, force_refresh_num, len(prev_asks),
+        len(prev_bids), len(ask_orders), len(bid_orders), len(mix_new_orders), )
+    logger.debug("put orders: %s, cancel orders num: %s", made_orders, cancel_num)
+
+
+async def market_making(
+    param: TokenParameter,        # market making parameters
+    ctx: dict,          # context, include previous orders
+    logger: Logger,
+    is_far: bool
+):
+    """ Run market making strategy
+        Parameters:
+            param: near end parameters of market making
+            far_param: far end parameters of market making
+            ctx: the context of the current symbol
+            logger: the logger
+            is_far: whether put far-end orders 
+    """
+    maker_symbol = param.maker_symbol
+    try:
+        job_start_ts = time.time()
+
+        # get order book of following symbol, cached in redis
+        ask_bid = DATA_REDIS_CLIENT.get_order_book(param) # TODO
+        if not ask_bid or not ask_bid.get('asks') or not ask_bid.get('bids'):
+            logger.warning('Cannot get quotes of %s', maker_symbol)
+            return
+
+        # first generate new near-end ask/bid orders
+        side = param.near_side # put ASK or BID or Both
+        new_asks = gen_ask_orders(ask_bid['asks'], param) if side in ('BOTH', 'ASK') else []
+        new_bids = gen_bid_orders(ask_bid['bids'], param) if side in ('BOTH', 'BID') else []
+
+        # for client order id
+        clorder_start = int(time.time() / 86400)
+        clorder_offset = int(time.time()*1000) % 86400000
+
+        # the top ask and bid of prevous round's near orders
+        top_bid = max(new_bids[0][0] if new_bids else float(ask_bid['bids'][0][0]), ctx.get('top_bid', 0))
+        valid_asks = []
+        for price, qty in new_asks:
+            if price > top_bid:  # avoid self-trade
+                valid_asks.append(
+                    # TODO: define batch order data structure
+                    BatchOrder(clorder_id=gen_client_order_id(
+                        maker_symbol, clorder_start, clorder_offset),
+                               symbol=maker_symbol,
+                               side="SELL",
+                               price=price,
+                               qty=qty,
+                               tif=param.near_tif))
+                clorder_offset += 1
+        valid_bids = []
+        top_ask = min(new_asks[0][0] if new_asks else float(ask_bid['asks'][0][0]), ctx.get('top_ask', top_bid))
+        for price, qty in new_bids:
+            if price < top_ask:
+                valid_bids.append(
+                    BatchOrder(clorder_id=gen_client_order_id(
+                        maker_symbol, clorder_start, clorder_offset),
+                               symbol=maker_symbol,
+                               side="BUY",
+                               price=price,
+                               qty=qty,
+                               tif=param.near_tif))
+                clorder_offset += 1
+        # put orders via exchange restful API
+        await handle_orders(param, maker_symbol, valid_asks, valid_bids, ctx, logger, False)
+        logger.info("[delay:maker-ner]%s:%s", maker_symbol, int(
+            (time.time() - job_start_ts) * 1000))
+
+        if not is_far:
+            return
+
+        # if updating the far put order,canecl all put order(far and ner)
+        far_ask_orders, far_bid_orders = [], []
+        job_start_ts = time.time()
+        if param.far_side in ('BOTH', 'ASK'):
+            far_ask_orders = gen_far_liquidity(maker_symbol, param, ask_bid, side='SELL',
+                                               guard_price=top_bid, cl_order_start=clorder_start)
+        if param.far_side in ('BOTH', 'BID'):
+            far_bid_orders = gen_far_liquidity(maker_symbol, param, ask_bid, side='BUY',
+                                               guard_price=top_ask, cl_order_start=clorder_start)
+        await handle_orders(
+            param, maker_symbol, far_ask_orders, far_bid_orders, ctx, logger, True)
+        logger.info("[delay:maker-far]%s:%s", maker_symbol, int(
+            (time.time() - job_start_ts) * 1000))
+    except Exception:
+        logger.error(traceback.format_exc())
+        await _clear_all_ner_open_orders(maker_symbol, ctx, logger)
+
+
+async def main(params: list[TokenParameter]):
+    """ The main function
+    """
+    logger = create_logger(CURR_PATH, f"market_making.log", 'MM')
+    logger.info('start market maker with config: %s', params)
+
+    _last_operating_ts = {}  # the timestamp of last making orders for each pair
+    _prev_context = {}  # previous context of MM data
+    while 1:
+        try:
+            tasks = []
+            for param in params:
+                symbol = param.maker_symbol
+                ts = time.time()
+
+                # check update frequency
+                if symbol not in _last_operating_ts:
+                    _last_operating_ts[symbol] = {'near_opts': 0.0, 'far_opts': 0.0}
+                op_ts = _last_operating_ts[symbol]
+                if op_ts['near_opts'] + param['interval'] > ts:
+                    continue
+                _last_operating_ts[symbol]['near_opts'] = ts
+
+                is_far = False
+                if param.far_interval and op_ts['far_opts'] + param.far_interval <= ts:
+                    _last_operating_ts[symbol]['far_opts'] = ts
+                    is_far = True
+
+                if symbol not in _prev_context:
+                    _prev_context[symbol] = {
+                        'client': '',       # TODO Rest API Client for order operations
+                        'prev_asks': [],    # previous made ask orders, near-end
+                        'prev_bids': [],    # previous made bid orders, near-end
+                        'prev_farasks': [],    # previous made ask orders, far-end
+                        'prev_farbids': [],    # previous made bid orders, far-end
+                        'no_force_refresh_num': 0,
+                    }
+                tasks.append(asyncio.create_task(market_making(
+                    param, _prev_context[symbol], logger, is_far)))
+
+            if tasks:
+                await asyncio.gather(*tasks)
+            else:
+                await asyncio.sleep(0.05)
+        except Exception:
+            logger.error(traceback.format_exc())
+
+    # before exit
+    for symbol, ctx in _prev_context.items():
+        await _clear_all_ner_open_orders(symbol, ctx, logger)
+
+
+if __name__ == '__main__':
+    params = [
+        TokenParameter({{
+            'Maker Symbol': 'btc_usdt'
+        }}),
+        TokenParameter({{
+            'Maker Symbol': 'dot_usdt'
+        }})
+    ]
+
+    asyncio.run(main(params))
