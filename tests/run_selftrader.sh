140152a0ae82ef06c19d2eb22264d7b944793729
King-Jump <github@0xjump.com>
2026-02-11 00:18:57 +0800

    MOD:

--- tests/run_selftrader.sh
+++ tests/run_selftrader.sh
@@ -1,5 +1,5 @@
 cd ~/bifu_finance/open_source/tuna-py
 source venv/bin/activate
-python3 tunapy/self_trader/self_trader.py tests/st_params_bn.json
-python3 tunapy/self_trader/self_trader.py tests/st_params_okx.json
+python3 tunapy/self_trader/self_trader.py tests/test_st_params_bn.json
+python3 tunapy/self_trader/self_trader.py tests/test_st_params_okx.json
 
