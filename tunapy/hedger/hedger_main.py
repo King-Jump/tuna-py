3ce7d930d5812caf39e27fc4cc7b961b9ee180e3
King-Jump <github@0xjump.com>
2026-02-25 16:41:34 +0800

    MOD: ws_client init parameter

--- tunapy/hedger/hedger_main.py
+++ tunapy/hedger/hedger_main.py
@@ -506,7 +506,7 @@ def main(conf: dict):
         logger.info('start hedger with config: %s', param)
         monitor = create_logger(BASE_PATH, "HedgeMonitor.log", 'monitor_hedger', backup_cnt=50)
         # Monitor BiFu trade executions
-        ws_client = BiFuPrivateWSClient(config['private_ws_client'], logger)
+        ws_client = BiFuPrivateWSClient(conf['private_ws_client'], logger)
         if not param.api_key or not param.api_secret:
             logger.error("Lost Hedge api key or secret")
             return
