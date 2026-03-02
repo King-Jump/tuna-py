6a19eee0b9efb4cdc47313d2b2c12d05bf60b0cf
King-Jump <github@0xjump.com>
2026-03-02 23:09:53 +0800

    MOD: set prefix directly in _key function

--- tunapy/quote/bn_public_ws.py
+++ tunapy/quote/bn_public_ws.py
@@ -34,7 +34,7 @@ def _key(tag, ts):
         The update period of Binance WS is 100ms.
         1 minutes have 60 * 10 = 600 (100ms)
     """
-    return f'{'binance_spot_depth'}{tag}{ts % ONE_MIN_HUNDRED_MS}'
+    return f'binance_spot_depth{tag}{ts % ONE_MIN_HUNDRED_MS}'
 
 def _handle_orderbook_depth(rkey: str, req_ts: int, data: dict) -> dict:
     order_book = {
@@ -52,7 +52,7 @@ def _handle_ticker(data: dict, req_ts: int) -> dict:
     symbol = data['data']['s'].lower()  # bn symbol default lowercase
     price = float(data['data']['p'])
     qty = float(data['data']['q'])
-    rkey = f'{'binance_spot_ticker'}{symbol}{req_ts % ONE_MIN_HUNDRED_MS}'
+    rkey = f'binance_spot_ticker{symbol}{req_ts % ONE_MIN_HUNDRED_MS}'
     DATA_REDIS_CLIENT.set_dict(f'{rkey}_value', {'price': price, 'qty': qty})
     DATA_REDIS_CLIENT.set_int(rkey, req_ts)
     LOGGER.info('Update Tick %s, price=%s, qty=%s', rkey, price, qty)
